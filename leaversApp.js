356a639e25f9d19d83a567021b17f20b39c4819b
Peter Irlam <irlampeter@gmail.com>
2022-07-15 10:52:02 +0100

    Update leavers app

--- leaversApp.js
+++ leaversApp.js
@@ -1,27 +1,27 @@
 function doPost(e) {
 
     if (typeof e !== 'undefined')
-        var role = e.parameter.role;
-    var workplace = e.parameter.workplace;
-    var team = e.parameter.team;
-    var name = e.parameter.name_001.replace(/^\s+|\s+$/g, '');
-    var email = e.parameter.email_001.replace(/^\s+|\s+$/g, '');
-    var phone = e.parameter.phone_001.replace(/^\s+|\s+$/g, '');
-    var leavingdate = e.parameter.leavingdate;
-    var startingdate = e.parameter.startingdate;
-    var addrline1 = e.parameter.addrline1_001.replace(/^\s+|\s+$/g, '');
-    var addrline2 = e.parameter.addrline2_001.replace(/^\s+|\s+$/g, '');
-    var town = e.parameter.town_001.replace(/^\s+|\s+$/g, '');
-    var county = e.parameter.county_001.replace(/^\s+|\s+$/g, '');
-    var postcode = e.parameter.postcode_001.replace(/^\s+|\s+$/g, '');
-    var reasonforleaving = e.parameter.reasonforleaving;
-    var outstandingexpenses = e.parameter.outstandingexpenses;
-    var charityequipment = e.parameter.charityequipment;
-    var days_in_post = e.parameter.days_in_post;
+        let role = e.parameter.role;
+    let workplace = e.parameter.workplace;
+    let team = e.parameter.team;
+    let name = e.parameter.name_001.replace(/^\s+|\s+$/g, '');
+    let email = e.parameter.email_001.replace(/^\s+|\s+$/g, '');
+    let phone = e.parameter.phone_001.replace(/^\s+|\s+$/g, '');
+    let leavingdate = e.parameter.leavingdate;
+    let startingdate = e.parameter.startingdate;
+    let addrline1 = e.parameter.addrline1_001.replace(/^\s+|\s+$/g, '');
+    let addrline2 = e.parameter.addrline2_001.replace(/^\s+|\s+$/g, '');
+    let town = e.parameter.town_001.replace(/^\s+|\s+$/g, '');
+    let county = e.parameter.county_001.replace(/^\s+|\s+$/g, '');
+    let postcode = e.parameter.postcode_001.replace(/^\s+|\s+$/g, '');
+    let reasonforleaving = e.parameter.reasonforleaving;
+    let outstandingexpenses = e.parameter.outstandingexpenses;
+    let charityequipment = e.parameter.charityequipment;
+    let days_in_post = e.parameter.days_in_post;
 
     sendLetter(role, workplace, team, name, email, phone, startingdate, leavingdate, addrline1, addrline2, town, county, postcode, reasonforleaving, outstandingexpenses, charityequipment, days_in_post);
 
-    var html = HtmlService.createTemplateFromFile('Thankyou');
+    let html = HtmlService.createTemplateFromFile('Thankyou');
 
     return html.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
         .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
@@ -29,19 +29,19 @@ function doPost(e) {
 }
 
 function getScriptUrl() {
-    var url = ScriptApp.getService().getUrl();
+    let url = ScriptApp.getService().getUrl();
     return url;
 }
 
 function doGet(e) {
-    var currentuser = Session.getActiveUser().getEmail();
-    var authusers = [someUser @someEmail.org.uk]; //users removed for anonimity
-    var isauthorised = authusers.indexOf(currentuser);
+    let currentuser = Session.getActiveUser().getEmail();
+    let authusers = [someUser@someEmail.org.uk]; //users removed for anonimity
+    let isauthorised = authusers.indexOf(currentuser);
 
     if (isauthorised >= 0) {
         return HtmlService.createHtmlOutputFromFile('Form');
     } else {
-        var html = HtmlService.createTemplateFromFile('Error');
+        let html = HtmlService.createTemplateFromFile('Error');
         html.currentuser = currentuser;
         return html.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
             .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
@@ -49,7 +49,7 @@ function doGet(e) {
 }
 
 function ordinal_suffix_of(i) {
-    var j = i % 10,
+    let j = i % 10,
         k = i % 100;
     if (j == 1 && k != 11) {
         return "st";
@@ -64,8 +64,8 @@ function ordinal_suffix_of(i) {
 }
 
 function getLongDateFormat(dateArray) {
-    var ordinal;
-    var day;
+    let ordinal;
+    let day;
     if (dateArray[0].substring(0, 1) == "0") {
         ordinal = ordinal_suffix_of(dateArray[0].substring(1))
         day = dateArray[0].substring(1);
@@ -73,7 +73,7 @@ function getLongDateFormat(dateArray) {
         ordinal = ordinal_suffix_of(dateArray[0])
         day = dateArray[0];
     }
-    var monthname;
+    let monthname;
     switch (dateArray[1]) {
 
         case "01":
@@ -128,7 +128,7 @@ function getLongDateFormat(dateArray) {
 }
 
 function sendLetter(role, workplace, team, name, email, phone, startingdate, leavingdate, addrline1, addrline2, town, county, postcode, reasonforleaving, outstandingexpenses, charityequipment, days_in_post) {
-    var roletype;
+    let roletype;
     switch (role) {
 
         case "Volunteer":
@@ -140,17 +140,17 @@ function sendLetter(role, workplace, team, name, email, phone, startingdate, lea
             break;
     }
 
-    var dstFolderId = DriveApp.getFolderById("1lVsjyYWp2VdvSqEF4lgfgMXF6kEK-FcN");
+    let dstFolderId = DriveApp.getFolderById("1lVsjyYWp2VdvSqEF4lgfgMXF6kEK-FcN");
 
-    var startdateArray = startingdate.split("/");
-    var leavingdateArray = leavingdate.split("/");
-    var startdate = new Date(startdateArray[2] + "/" + startdateArray[1] + "/" + startdateArray[0]);
-    var leavingdate = new Date(leavingdateArray[2] + "/" + leavingdateArray[1] + "/" + leavingdateArray[0]);
-    var todayArray = Utilities.formatDate(new Date(), "GMT+1", "dd/MM/yyyy").split("/");;
-    var leavingdateLongFormat = getLongDateFormat(leavingdateArray);
-    var todayLongFormat = getLongDateFormat(todayArray);
+    let startdateArray = startingdate.split("/");
+    let leavingdateArray = leavingdate.split("/");
+    let startdate = new Date(startdateArray[2] + "/" + startdateArray[1] + "/" + startdateArray[0]);
+    let leavingdate = new Date(leavingdateArray[2] + "/" + leavingdateArray[1] + "/" + leavingdateArray[0]);
+    let todayArray = Utilities.formatDate(new Date(), "GMT+1", "dd/MM/yyyy").split("/");;
+    let leavingdateLongFormat = getLongDateFormat(leavingdateArray);
+    let todayLongFormat = getLongDateFormat(todayArray);
 
-    var address;
+    let address;
     if (addrline2.length > 0) {
         if (county.length > 1) {
             address = addrline1 + '\r' + addrline2 + '\r' + town + '\r' + county + '\r' + postcode;
@@ -168,10 +168,10 @@ function sendLetter(role, workplace, team, name, email, phone, startingdate, lea
 
     if (days_in_post >= 180) {
 
-        var docid = DriveApp.getFileById("1n3zWEttQA8LiwADEBFH-a5oHzU6ycGBhz0KM3l_d2fs").makeCopy("Leavers_Letter_" + Utilities.formatDate(new Date(), "GMT+1", "dd-MMM-yyyy") + "_" + name, dstFolderId).getId()
-        var doc = DocumentApp.openById(docid);
+        let docid = DriveApp.getFileById("1n3zWEttQA8LiwADEBFH-a5oHzU6ycGBhz0KM3l_d2fs").makeCopy("Leavers_Letter_" + Utilities.formatDate(new Date(), "GMT+1", "dd-MMM-yyyy") + "_" + name, dstFolderId).getId()
+        let doc = DocumentApp.openById(docid);
 
-        var body = doc.getActiveSection();
+        let body = doc.getActiveSection();
         body.replaceText("%fullname%", name);
         body.replaceText("%addrline1%", address);
         body.replaceText("%town%", town);
@@ -194,8 +194,8 @@ function sendLetter(role, workplace, team, name, email, phone, startingdate, lea
         //GmailApp.sendEmail('email hidden', 'Leaver Notification Letter - TEST PLEASE IGNORE', 'Please see the attached letter.', {attachments: [doc.getAs(MimeType.PDF)],name: 'Leaver Notification - TEST PLEASE IGNORE' });
     }
 
-    var docid2 = DriveApp.getFileById("1dm7XshpRE1jsHhUkV45X61-eYA1IjW6ut2H6g1mxhzE").makeCopy("Leavers_Report_" + Utilities.formatDate(new Date(), "GMT+1", "dd-MMM-yyyy") + "_" + name, dstFolderId).getId();
-    var doc2 = DocumentApp.openById(docid2);
+    let docid2 = DriveApp.getFileById("1dm7XshpRE1jsHhUkV45X61-eYA1IjW6ut2H6g1mxhzE").makeCopy("Leavers_Report_" + Utilities.formatDate(new Date(), "GMT+1", "dd-MMM-yyyy") + "_" + name, dstFolderId).getId();
+    let doc2 = DocumentApp.openById(docid2);
 
     if (phone.length < 1) {
         phone = "Not recorded";
@@ -209,7 +209,7 @@ function sendLetter(role, workplace, team, name, email, phone, startingdate, lea
     if (charityequipment.length < 1) {
         charityequipment = "None stated";
     }
-    var body2 = doc2.getActiveSection();
+    let body2 = doc2.getActiveSection();
 
     body2.replaceText("%fullname%", name);
     body2.replaceText("%addrline1%", address)
